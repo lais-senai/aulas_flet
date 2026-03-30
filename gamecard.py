63d5ee01727b910481ac0136e50af3630f2c181c
lais-senai <lais.v.pereira@aluno.senai.br>
2026-03-30 16:15:19 -0300

    commit

--- gamecard.py
+++ gamecard.py
@@ -1,5 +1,6 @@
 import flet as ft
 
+
 def main(page: ft.Page):
     # Configurações da Janela
     page.title = "Gamer Card"
@@ -8,14 +9,15 @@ def main(page: ft.Page):
     page.vertical_alignment = ft.MainAxisAlignment.CENTER
 
     # Cabeçalho: Avatar (Emoji) e Nome do Lado
-
     cabecalho = ft.Row(
         controls=[
-            ft.Text("🥸", size=60), #Emoji a prova de erros
+            ft.Text("👑", size=60),  # Emoji a prova de erros
             ft.Column(
                 controls=[
-                    ft.Text("SHADOW_NINJA", size=24, weight="bold", color="white"),
-                    ft.Text("NIvel 42 - Ninja das Sombras", size=24, color="grey300"),
+                    ft.Text("PRINCESS", size=24,
+                            weight="bold", color="white"),
+                    ft.Text("Nível 100 - Princesa porpurina",
+                            size=24, color="grey300"),
                 ], spacing=2
             )
         ],
@@ -23,6 +25,58 @@ def main(page: ft.Page):
     )
 
     # Status: Barra de HP, MP e XP
+    status_bar = ft.Row(
+        controls=[
+            ft.Container(
+                content=ft.Text(
+                    "HP: 100", text_align=ft.TextAlign.CENTER, weight="bold"),
+                bgcolor="red400", padding=10, border_radius=8, expand=4
+            ),
+            ft.Container(
+                content=ft.Text(
+                    "MP: 50", text_align=ft.TextAlign.CENTER, weight="bold"),
+                bgcolor="blue400", padding=10, border_radius=8, expand=1
+            ),
+            ft.Container(
+                content=ft.Text(
+                    "XP: 9999", text_align=ft.TextAlign.CENTER, weight="bold"),
+                bgcolor="amber400", padding=10, border_radius=8, expand=3
+            ),
+        ],
+        spacing=10
+    )
 
-
+    # BOTÕES: Ações do jogador
+    botoes_acao = ft.Row(
+        controls=[
+            ft.Button(
+                content="Adicionar Amigo",
+                bgcolor="#8E368A",
+                color="white"
+            ),
+            ft.Button(
+                content="Batalha de Glitter",
+                bgcolor="pink",
+                color="white"
+            ),
+        ],
+           alignment=ft.MainAxisAlignment.CENTER,
+           spacing=20 
+    )
+    
+    #CARTÃO PRINCIPAL
+    cartao = ft.Container(
+        content=ft.Column(
+            controls=[cabecalho, status_bar, botoes_acao],
+            spacing=30
+        ),
+        bgcolor="#2E2E2E",
+        padding=40,
+        border_radius=15,
+        width=450,
+        shadow=ft.BoxShadow(blur_radius=20, color="white") #aqui vai ser um efeito de sombra ok?
+    )
+    
+    page.add(cartao)
+    
 ft.run(main)
